f92d3bbc89439213bf3c0d180aeb123baa4087e2
JimmyRoselin <j.roselin@hotmail.com>
2018-05-10 15:29:28 +0200

    css and buttons
    ayyy

--- Kandidathantering/Kandidathantering/success.php
+++ Kandidathantering/Kandidathantering/success.php
@@ -1,8 +1,5 @@
 <?php
-
-
-
-
+//Kontrollerar om det finns en cookie, annars skickas användaren tillbaka till login-sidan
 ?>
 <html>
     <head>
@@ -12,16 +9,29 @@
         <title></title>
     </head>
     <body>
-<?php
-
+        <?php
 //Presenterar för- och efternamn som hämtats från API'n och lagrats i cookien
 
-echo "<h1>Välkommen, " . $_SESSION['user']['firstname']. " " . $_SESSION['user']['lastname'] . "!</h1>";
-echo '<br>';
+        echo "<h1>Välkommen!</h1>";
+        echo "<h2>" . $_SESSION['user']['firstname'] . " " . $_SESSION['user']['lastname'] . "!</h2>";
+        ?>
 
-?>
-        <br>  <a href="logout.php">Log out!</a> <br>
 
+        <!--        <br>  <a href="logout.php">Log out!</a> <br>-->
+        </br>
+        <img class="strategBlack" src="content/bilder/Strateg_liggande-SVART_1.png" alt="strateg"/>
+        <img class="strategWhite" src="content/bilder/Strateg_liggande-VIT_utan_text.png" alt="strateg"/>
+        </br
+
+
+        <form action="updateprofileform.php">
+            <input type="submit" value="Redigera profil" />
+        </form>
+        </br>        </br>
+
+        <form action="logout.php">
+            <input type="submit" value="Logga ut" />
+        </form>
 
 
 
