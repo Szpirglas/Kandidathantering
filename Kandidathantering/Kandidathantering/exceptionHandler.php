72e9fe3f91d6a29475bf192b1f2e2310224f39b8
Mattias Vuolo Sandin <mattias@sandinfoto.se>
2018-05-15 10:45:41 +0200

    undantagshantering
    ta-fuckin'-da!

---
+++ Kandidathantering/Kandidathantering/exceptionHandler.php
@@ -0,0 +1,34 @@
+<?php
+
+// Hämta .env-filen
+
+require __DIR__ . '/vendor/autoload.php';
+
+$dotenv = new Dotenv\Dotenv(__DIR__);
+$dotenv->load();
+
+require_once 'dbConnection.php';
+
+class ExceptionHandler {
+
+    protected $dbConnect;
+
+    function __construct() {
+
+        $this->dbConnect = new dbConnection();
+    }
+
+    function addException($vid, $url, $exception) {
+
+        $connect = $this->dbConnect->connect();
+        
+        $time = date("Y-m-d H:i:s");
+
+        $query = "INSERT INTO EXCEPTION (USERID, URL, EXMSG, DATE) VALUES ($vid, '$url', '$exception->getMessage()', $time)";
+        
+        $connect->query($query);
+        
+        header("Location: error.php");
+    }
+
+}
